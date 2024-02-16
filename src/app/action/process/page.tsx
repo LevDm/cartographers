7db058ef908a29d2696d8a43dbf2fdc6562b5894
LevDm <new email>
2024-02-16 12:04:45 +0500

    feat: диалоговое окно завершения игры

--- src/app/action/process/page.tsx
+++ src/app/action/process/page.tsx
@@ -249,8 +249,7 @@ type ActionBarPropsType = {
 };
 
 function ActionBar(props: ActionBarPropsType) {
-  const { gameState, actionBarHandler } = props;
-  const { season, freeSkills } = gameState;
+  const { actionBarHandler } = props;
 
   return (
     <Box
@@ -272,17 +271,64 @@ function ActionBar(props: ActionBarPropsType) {
         <AddIcon sx={{ mr: 1 }} />
         Ход
       </Fab>
-      <Fab
-        variant="extended"
-        size="medium"
-        onClick={() =>
-          actionBarHandler({ action: "season", value: season !== 3 ? "next" : "finish" })
-        }
-      >
-        <NavigationIcon sx={{ mr: 1 }} />
-        {season !== 3 ? SEASONS[season + 1].title : "Закончить"}
-      </Fab>
+      <SeasonFab {...props} />
     </Box>
+  );
+}
+
+interface SeasonFabPropsType {
+  gameState: GameStateType;
+  actionBarHandler: (e: { action: AllActionTypes; value?: string }) => unknown;
+}
+function SeasonFab(props: SeasonFabPropsType) {
+  const { gameState, actionBarHandler } = props;
+
+  const { season } = gameState;
+
+  const title = season !== 3 ? SEASONS[season + 1].title : "Закончить";
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    if (season == 3) setOpen(true);
+    else actionBarHandler({ action: "season", value: "next" });
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleApply = () => {
+    handleClose();
+    actionBarHandler({ action: "season", value: "finish" });
+  };
+
+  return (
+    <>
+      <Fab variant="extended" size="medium" onClick={handleClickOpen}>
+        <NavigationIcon sx={{ mr: 1 }} />
+        {title}
+      </Fab>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="finish-dialog-title"
+        aria-describedby="finish-dialog-description"
+      >
+        <DialogTitle id="finish-dialog-title">Закончить игру?</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="finish-dialog-description">
+            После подтерждения возвращение на эту страницу будет недоступно
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Отмена</Button>
+          <Button onClick={handleApply} autoFocus>
+            Ок
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
