32da3c010c49da5ff1c0a76e22d0d708d799dbc9
LevDm <new email>
2024-02-27 13:43:57 +0500

    feat: дизайн главной страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,12 @@ import {
   Typography,
   Stack,
   Container,
+  Paper,
+  Divider,
 } from "@mui/material";
+
+import CoverBG from "../../public/cover.jpg";
+import Image from "next/image";
 
 import { observer } from "mobx-react-lite";
 
@@ -48,21 +53,66 @@ const HomePage = observer(() => {
         </Toolbar>
       </AppBar>
 
-      <Container component={"section"}>
-        <Stack spacing={2}>
-          <Button size="large" onClick={() => link("/action")}>
-            Новая
-          </Button>
-          <Button
-            size="large"
-            onClick={() => link("/action/process")}
-            disabled={!(loadSuccses.get() && lastSave.get() != null)}
-          >
-            {lastSave.get()
-              ? `${format(lastSave.get() ?? "", "HH:mm / dd.MM")} - продолжить`
-              : "Нет сохранений"}
-          </Button>
-        </Stack>
+      <Image
+        src={CoverBG}
+        alt=""
+        style={{
+          marginTop: "56px",
+          position: "absolute",
+          zIndex: -1,
+          width: "100%",
+          height: "100%",
+          opacity: 0.9,
+        }}
+        objectFit="cover"
+        fill={true}
+        placeholder="blur"
+      />
+
+      <Container
+        component={"section"}
+        sx={{
+          height: "93vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Paper
+          elevation={0}
+          sx={{
+            padding: 5,
+            paddingTop: 3,
+            backgroundColor: "rgba(255,255,255,0.76)",
+            backdropFilter: "blur(5px)",
+            borderRadius: 4,
+          }}
+        >
+          <Stack spacing={2} alignItems={"center"}>
+            <Typography variant="h5" fontWeight={"bold"}>
+              Запуск
+            </Typography>
+            <Divider orientation="horizontal" flexItem />
+
+            <Button
+              size="large"
+              sx={{ textTransform: "none", width: "100%" }}
+              onClick={() => link("/action")}
+            >
+              Новый
+            </Button>
+            <Button
+              size="large"
+              sx={{ textTransform: "none" }}
+              onClick={() => link("/action/process")}
+              disabled={!(loadSuccses.get() && lastSave.get() != null)}
+            >
+              {lastSave.get()
+                ? `${format(lastSave.get() ?? "", "HH:mm / dd.MM")} - продолжить`
+                : "Нет сохранений"}
+            </Button>
+          </Stack>
+        </Paper>
       </Container>
     </Box>
   );
